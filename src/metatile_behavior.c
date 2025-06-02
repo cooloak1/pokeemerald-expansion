4bb04e45292ca9ac4e93674c97039548cfd7912e
cooloak1 <92229864+cooloak1@users.noreply.github.com>
2025-06-02 13:38:03 -0500

    remove pond reflections

--- src/metatile_behavior.c
+++ src/metatile_behavior.c
@@ -206,14 +206,14 @@ bool8 MetatileBehavior_IsDeepSand(u8 metatileBehavior)
 
 bool8 MetatileBehavior_IsReflective(u8 metatileBehavior)
 {
-    if (metatileBehavior == MB_POND_WATER
-     || metatileBehavior == MB_PUDDLE
-     || metatileBehavior == MB_UNUSED_SOOTOPOLIS_DEEP_WATER_2
-     || metatileBehavior == MB_ICE
-     || metatileBehavior == MB_SOOTOPOLIS_DEEP_WATER
-     || metatileBehavior == MB_REFLECTION_UNDER_BRIDGE)
-        return TRUE;
-    else
+//    if (metatileBehavior == MB_POND_WATER
+//     || metatileBehavior == MB_PUDDLE
+//     || metatileBehavior == MB_UNUSED_SOOTOPOLIS_DEEP_WATER_2
+//     || metatileBehavior == MB_ICE
+//     || metatileBehavior == MB_SOOTOPOLIS_DEEP_WATER
+//     || metatileBehavior == MB_REFLECTION_UNDER_BRIDGE)
+//        return TRUE;
+//    else
         return FALSE;
 }
 
